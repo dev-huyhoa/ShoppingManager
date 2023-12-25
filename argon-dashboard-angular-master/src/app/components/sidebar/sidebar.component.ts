6697dda3e4b5823c7b2ab0bafdc0bf3e60415895
NgThien <thien.vu@finfan.vn>
2023-12-25 22:49:34 +0700

    ẩn sidebar

--- argon-dashboard-angular-master/src/app/components/sidebar/sidebar.component.ts
+++ argon-dashboard-angular-master/src/app/components/sidebar/sidebar.component.ts
@@ -9,16 +9,16 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Thống kê',  icon: 'ni-tv-2 text-primary', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/list-employee', title: 'Nhân viên',  icon:'ni ni-circle-08 text-purple', class: '' },
-    { path: '/list-customer', title: 'Khách Hàng',  icon:'ni ni-single-02 text-purple', class: '' },
-    { path: '/list-role', title: 'Chức vụ',  icon:'fa-solid fa-key text-purple', class: '' },
-    { path: '/list-category', title: 'Danh Mục',  icon:'fa-solid fa-list text-purple', class: '' },
+    // { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
+    // { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
+    // { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
+    // { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
     { path: '/list-banner', title: 'Banner',  icon:'ni ni-image text-purple', class: '' },
-    { path: '/list-product', title: 'Product',  icon:'fa-solid fa-shirt text-purple', class: '' },
+    { path: '/list-role', title: 'Chức vụ',  icon:'fa-solid fa-key text-purple', class: '' },
+    { path: '/list-employee', title: 'Nhân viên',  icon:'ni ni-circle-08 text-purple', class: '' },
+    { path: '/list-customer', title: 'Khách hàng',  icon:'ni ni-single-02 text-purple', class: '' },
+    { path: '/list-category', title: 'Danh mục',  icon:'fa-solid fa-list text-purple', class: '' },
+    { path: '/list-product', title: 'Sản phẩm',  icon:'fa-solid fa-shirt text-purple', class: '' },
 
 ];
 
